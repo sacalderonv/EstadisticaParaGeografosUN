b0ce67cd61cb381ded9cfa2012d253cc3cfa4060
Sergio Alejandro Calderon Villanueva <55386964+sacalderonv@users.noreply.github.com>
2023-09-04 15:01:34 -0500

    Actualización

---
+++ Introduccion_a_R _Version_1/VariablesAletoriasDiscretas/MediayVarianza.R
@@ -0,0 +1,5 @@
+##Aquí se calcula la media y la varianza poblacionales del ejemplo----
+media1=0.15*0 + 0.35*1 + 0.30*2 + 0.15*3 +0.05*4
+media2= 0.20*0+ 0.20*1 + 0.30*2 + 0.15*3 +0.15*4
+varianza1=0.15*(0-media1)^2 + 0.35*(1-media1)^2 + 0.30*(2-media1)^2 + 0.15*(3-media1)^2 +0.05*(4-media1)^2
+varianza2=0.15*(0-media2)^2 + 0.35*(1-media2)^2 + 0.30*(2-media2)^2 + 0.15*(3-media2)^2 +0.05*(4-media2)^2
