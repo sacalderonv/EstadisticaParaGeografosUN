982cf5ff0930d68161da0ce4e34c2fd5b04136af
Sergio Alejandro Calderon Villanueva <55386964+sacalderonv@users.noreply.github.com>
2023-09-10 08:10:17 -0500

    Actualizacion

--- Introduccion_a_R _Version_1/VariablesAletoriasDiscretas/MediayVarianza.R
+++ Introduccion_a_R _Version_1/VariablesAletoriasDiscretas/MediayVarianza.R
@@ -2,7 +2,7 @@
 media1=0.15*0 + 0.35*1 + 0.30*2 + 0.15*3 +0.05*4
 media2= 0.20*0+ 0.20*1 + 0.30*2 + 0.15*3 +0.15*4
 varianza1=0.15*(0-media1)^2 + 0.35*(1-media1)^2 + 0.30*(2-media1)^2 + 0.15*(3-media1)^2 +0.05*(4-media1)^2
-varianza2=0.15*(0-media2)^2 + 0.35*(1-media2)^2 + 0.30*(2-media2)^2 + 0.15*(3-media2)^2 +0.05*(4-media2)^2
+varianza2=0.2*(0-media2)^2 + 0.2*(1-media2)^2 + 0.30*(2-media2)^2 + 0.15*(3-media2)^2 +0.15*(4-media2)^2
 
 ###
 media1
@@ -14,10 +14,18 @@ varianza2
 ## Otra forma de calcularlos es como sigue:
 x=c(0,1,2,3,4)
 p_x=c(0.15,0.35,0.3,0.15,0.05)
+p_x1=c(0.20, 0.20, 0.30,0.15,0.15)
 media_1=sum(x*p_x)
 varianza_1=sum(((x-media_1)^2)*p_x)
 media_1
 varianza_1
+
+####Región 2
+
+media_2=sum(x*p_x1)
+varianza_2=sum(((x-media_2)^2)*p_x1)
+media_2
+varianza_2
 
 ##Gráfico de la función de distribución ----
 
