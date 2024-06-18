b492f46ca5c8d2453cd567d0e280c0a8b836f590
Sergio Alejandro Calderon Villanueva <55386964+sacalderonv@users.noreply.github.com>
2024-06-18 13:16:20 -0500

    Actualización

--- Estadistica_Descriptiva/Mapas/DescriptivaMapas.R
+++ Estadistica_Descriptiva/Mapas/DescriptivaMapas.R
@@ -11,8 +11,12 @@ Ocupados_ENH <- read_excel("Ocupados_ENH.xlsx",
                                              "numeric", "numeric", "numeric"))
 
 Media_Ing<-Ocupados_ENH%>%group_by(DPTO)%>%summarise(Media_Ing=mean(INGLABO))
+Prop_Contrat.1<-Ocupados_ENH%>%group_by(DPTO,P6440)%>%summarise(n=n())%>%
+  mutate(freq = n / sum(n))%>%filter(P6440=="1")%>%dplyr::select(DPTO,freq)%>%rename(freq.1="freq")
+Prop_Contrat.2<-Ocupados_ENH%>%group_by(DPTO,P6440)%>%summarise(n=n())%>%
+  mutate(freq = n / sum(n))%>%filter(P6440=="2")%>%dplyr::select(DPTO,freq)%>%rename(freq.2="freq")
 
-#Mapa del Indice de Ruralidad Departamentos ----
+Media_ING_Prop_Contract<-Media_Ing%>%left_join(Prop_Contrat.1,by=c("DPTO"="DPTO"))%>%left_join(Prop_Contrat.2,by=c("DPTO"="DPTO"))
 deptos <- Municipios %>%
   group_by(Departamento,Dep) %>%
   summarise(Irural=sum(Poblacion*Irural)/sum(Poblacion)) %>%
@@ -27,7 +31,7 @@ str(deptoshp)
 mapdeptos <- deptoshp %>% left_join(deptos,by=c("DPTO_CCDGO"="Dep"))
 str(mapdeptos)
 
-mapdeptos_Ing <- deptoshp %>% left_join(Media_Ing,by=c("DPTO_CCDGO"="DPTO"))
+mapdeptos_Ing_Prop <- deptoshp %>% left_join(Media_ING_Prop_Contract,by=c("DPTO_CCDGO"="DPTO"))
 str(mapdeptos)
 
 
@@ -59,11 +63,25 @@ ggplot() +
 ####Con Ingreso Laboral ----
 ggplot() +
   geom_sf(data=mundocol) +
-  geom_sf(data=mapdeptos_Ing,aes(fill=Media_Ing),col="darkgray",linetype="solid") +
+  geom_sf(data=mapdeptos_Ing_Prop,aes(fill=Media_Ing),col="darkgray",linetype="solid") +
   coord_sf(xlim=c(box$xmin,box$xmax),ylim=c(box$ymin,box$ymax),expand=FALSE) +
-  geom_sf_text(data=mapdeptos_Ing,aes(label=ifelse(Media_Ing < 1300000,DPTO_CNMBR,"")),col="black",
+  geom_sf_text(data=mapdeptos_Ing_Prop,aes(label=ifelse(Media_Ing < 1300000,DPTO_CNMBR,"")),col="black",
                fontface="bold",size=4,fun.geometry=function(x) sf::st_centroid(x)) +
   labs(x="Longitud",y="Latitud",title="Colombia",fill="Ingreso promedio Laboral") +
+  scale_fill_gradient(low="white",high="red",n.breaks=5) +
+  annotate("text", x=c(-74.5,-68,-78,-69,-78.5), y=c(-2.5,0,-1,9,9), colour="blue",
+           label=c("Perú","Brasil","Ecuador","Venezuela","Panamá")) +
+  theme(panel.background=element_rect(fill="lightblue"))
+
+
+####Con Proporción Contrato Laboral ----
+ggplot() +
+  geom_sf(data=mundocol) +
+  geom_sf(data=mapdeptos_Ing_Prop,aes(fill=freq.1),col="darkgray",linetype="solid") +
+  coord_sf(xlim=c(box$xmin,box$xmax),ylim=c(box$ymin,box$ymax),expand=FALSE) +
+  geom_sf_text(data=mapdeptos_Ing_Prop,aes(label=ifelse(freq.1 > 0.5,DPTO_CNMBR,"")),col="black",
+               fontface="bold",size=2,fun.geometry=function(x) sf::st_centroid(x)) +
+  labs(x="Longitud",y="Latitud",title="Colombia",fill="Proporción Contrato Laboral") +
   scale_fill_gradient(low="white",high="red",n.breaks=5) +
   annotate("text", x=c(-74.5,-68,-78,-69,-78.5), y=c(-2.5,0,-1,9,9), colour="blue",
            label=c("Perú","Brasil","Ecuador","Venezuela","Panamá")) +
@@ -82,22 +100,8 @@ str(cund)
 ggplot() +
   geom_sf(data=cund,aes(fill=Irural),col="darkgray",linetype="solid") +
   geom_sf_text(data=cund,aes(label=ifelse(Irural > quantile(Irural,0.95),Municipio,"")),col="black",
-               fontface="bold",size=3.5,fun.geometry=function(x) sf::st_centroid(x)) +
+               fontface="bold",size=2,fun.geometry=function(x) sf::st_centroid(x)) +
   labs(x="Longitud",y="Latitud",title="Cundinamarca",fill="Índice de\nRuralidad") +
-  scale_fill_gradient(low="white",high="red",n.breaks=5)
-
-
-datos <- mpioshp %>%
-  left_join(Municipios,by=c("MPIO_CCNCT"="Depmun")) %>%
-  filter(Dep=="05")
-Deps <- unique(datos$Departamento)
-
-ggplot() +
-  geom_sf(data=datos,aes(fill=Irural),col="darkgray",linetype="solid") +
-  geom_sf_text(data=datos,aes(label=ifelse(Irural > quantile(Irural,0.95),Municipio,"")),col="black",
-               fontface="bold",size=3.5,fun.geometry=function(x) sf::st_centroid(x)) +
-  labs(x="Longitud",y="Latitud",title=ifelse(length(Deps)==1,Deps,paste0(Deps,collapse=" y ")),
-       fill="Índice de\nRuralidad") +
   scale_fill_gradient(low="white",high="red",n.breaks=5)
 
 
@@ -132,9 +136,10 @@ ggplot() +
   scale_fill_gradient(low="white",high="red",n.breaks=5)
 
 
-###Boyacá Estacionaes de Monitoreo Precipitación ----
+###Boyacá Estaciones de Monitoreo Precipitación ----
+library(stringr)
 Estaciones <- read_excel("Estaciones.xlsx")
-Estaciones <- within(Estaciones,Depmun <- str_sub(Depmun,1,5))
+Estaciones <- within(Estaciones,Depmun <- stringr::str_sub(Depmun,1,5))
 str(Estaciones)
 
 
